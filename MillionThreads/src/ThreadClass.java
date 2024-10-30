7057b9bdbe75b66ea89fbd74e8ba707030b05ad5
anishvankadhara <anishv0821@gmail.com>
2024-10-30 12:24:48 -0400

    TestingThreadClass
    testing adding the treadclass

---
+++ MillionThreads/src/ThreadClass.java
@@ -0,0 +1,12 @@
+public class ThreadClass {
+
+    public void run()
+    {
+        try {
+        }
+        catch (Exception e) {
+            // Throwing an exception
+            System.out.println("Exception is caught");
+        }
+    }
+}
